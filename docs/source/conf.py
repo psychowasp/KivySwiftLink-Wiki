b5149b6e0e262bf468dc01ca0286a6cbc9d722fe
psychowasp <psychowaspx@gmail.com>
2022-01-27 11:40:45 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'KivySwiftLink'
+copyright = '2021, PsycHoWasP'
+author = 'PsycHoWasP'
 
 release = '0.1'
 version = '0.1.0'
