f68864cbb112565678e2b707b7b7c311d26d05d7
PsycHoWasP <psychowaspx@gmail.com>
2022-03-11 22:48:34 +0100

    Update swift_types.py

--- simpleble/swift_types.py
+++ simpleble/swift_types.py
@@ -1,46 +1,46 @@
 
-from typing import List,Tuple,TypeVar
-from enum import Enum
-from collections.abc import Sequence as sequence
+# from typing import List,Tuple,TypeVar
+# from enum import Enum
+# from collections.abc import Sequence as sequence
 #from ctypes import c_int8 as
 
 
-__all__ = [
-    "long",
-    "ulong",
-    "longlong",
-    "ulonglong",
-    "uint8",
-    "int16",
-    "uint16",
-    "int32",
-    "uint32",
-    "data",
-    "json",
-    "jsondata",
-    "uint",
-    "double",
-    "float32",
-    ## other types
-    "TypeVar",
-    "struct",
-    "List",
-    "Tuple",
-    "callback",
-    "call_class",
-    "call_target",
-    "swift_func",
-    "EventDispatcher",
-    "Codable",
-    "direct",
-    "wrapper",
-    "python",
-    "Enum",
-    "send_self",
-    "sequence",
-    "ObjectStrEnum",
-    "ObjectIntEnum"
-    ]
+# __all__ = [
+#     "long",
+#     "ulong",
+#     "longlong",
+#     "ulonglong",
+#     "uint8",
+#     "int16",
+#     "uint16",
+#     "int32",
+#     "uint32",
+#     "data",
+#     "json",
+#     "jsondata",
+#     "uint",
+#     "double",
+#     "float32",
+#     ## other types
+#     "TypeVar",
+#     "struct",
+#     "List",
+#     "Tuple",
+#     "callback",
+#     "call_class",
+#     "call_target",
+#     "swift_func",
+#     "EventDispatcher",
+#     "Codable",
+#     "direct",
+#     "wrapper",
+#     "python",
+#     "Enum",
+#     "send_self",
+#     "sequence",
+#     "ObjectStrEnum",
+#     "ObjectIntEnum"
+#     ]
 
 int32 = object
 uint32 = object
@@ -62,37 +62,37 @@ float32 = object
 longdouble = object
 
 
-def EventDispatcher(_: list[str]): ...
+# def EventDispatcher(_: list[str]): ...
 
-def callback(direct: bool): ...
+# def callback(direct: bool): ...
 
-def call_class(class_name: str): ...
+# def call_class(class_name: str): ...
 
-def call_target(class_name: str): ...
+# def call_target(class_name: str): ...
 
-def swift_func(): ...
+# def swift_func(): ...
 
-def _direct(): ...
+# def _direct(): ...
 
-#def codable(): ...
+# #def codable(): ...
 
-class Codable: ...
+# class Codable: ...
 
-#def wrapper(dispatch_events: bool = False, events: list[str] = [], singleton: bool = True): ...
+# #def wrapper(dispatch_events: bool = False, events: list[str] = [], singleton: bool = True): ...
 
-class wrapper:
+# class wrapper:
     
-    def __init__(self, *args, **kwargs): ...
+#     def __init__(self, *args, **kwargs): ...
     
-    def __call__(self, *args, **kwargs): ...
+#     def __call__(self, *args, **kwargs): ...
 
-def python(): ...
+# def python(): ...
 
 
-def send_self(): ...
+# def send_self(): ...
 
-ObjectStrEnum = Enum
+# ObjectStrEnum = Enum
 
-ObjectIntEnum = Enum
+# ObjectIntEnum = Enum
 
-direct = _direct()
+# direct = _direct()
