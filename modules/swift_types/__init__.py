ad2acaa70f9136729cb648a9500f506eca808cab
PsycHoWasP <psychowaspx@gmail.com>
2022-03-12 15:02:54 +0100

    Update __init__.py

--- modules/swift_types/__init__.py
+++ modules/swift_types/__init__.py
@@ -6,13 +6,6 @@ from function_decorators import *
 
 from class_decorators import *
 
-#import signed_integers
-#import unsigned_integers
-import sequences
-import function_decorators
-import class_decorators
-
-from function_decorators import FunctionDecorators
 
 
 callback = FunctionDecorators.callback
