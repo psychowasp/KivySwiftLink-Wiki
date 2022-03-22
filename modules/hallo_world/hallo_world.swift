162250d5c9940548eb8ffc58b33b1bd772fdc973
PsycHoWasP <psychowaspx@gmail.com>
2022-03-22 10:32:49 +0100

    .

--- modules/hallo_world/hallo_world.swift
+++ modules/hallo_world/hallo_world.swift
@@ -1,17 +1,12 @@
 import Foundation
 
 class HalloWorld {
-    var py: HalloWorldPyCallback!
     init() {
         InitHalloWorld_Delegate(delegate: self)
     }
 }
 
 extension HalloWorld: HalloWorld_Delegate {
-    func set_HalloWorld_Callback(callback: HalloWorldPyCallback, cython_class: CythonClass) {
-        py = callback
-    }
-
 
     func hello_world(string: String) {
         print(string)
